9d1a1377ff796718d3d243fb914eb1eb4c7ef55d
daminimali <148991044+daminimali@users.noreply.github.com>
2024-06-06 21:38:23 +0530

    Update and rename Largest element in an array to LargestElementInAnArray.java

--- Problems on array/easy /LargestElementInAnArray.java
+++ Problems on array/easy /LargestElementInAnArray.java
@@ -1,4 +1,4 @@
-**Examples**
+/* *Examples**
 Example 1:
 Input:
  arr[] = {2,5,1,3,0};
@@ -30,8 +30,7 @@ Before sorting: arr[] = {2,5,1,3,0};
 After sorting: arr[] = {0,1,2,3,5};
 
 Hence answer : arr[sizeofthearray-1] =5
-
-Code:
+*/
 import java.util.*;
 public class tuf {
  
@@ -48,7 +47,7 @@ public class tuf {
     return arr[arr.length - 1];
   }
 }
-
+/*
 Output:
 
 The Largest element in the array is: 5
@@ -70,7 +69,7 @@ Create a max variable and initialize it with arr[0].
 Use a for loop and compare it with other elements of the array
 If any element is greater than the max value, update max value with the element’s value
 Print the max variable.
-
+*/
 import java.util.*;
 public class tuf {
  
@@ -94,7 +93,7 @@ public class tuf {
     return max;
   }
 }
-
+/*
 Output:
 
 The largest element in the array is: 5
@@ -104,3 +103,4 @@ Complexity Analysis
 Time Complexity: O(N)
 
 Space Complexity: O(1)
+*/
