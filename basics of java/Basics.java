fc36ebf1e1cc53af939fbd7124961d623acde98c
daminimali <148991044+daminimali@users.noreply.github.com>
2024-06-06 21:29:00 +0530

    Update and rename basics to Basics.java

--- basics of java/Basics.java
+++ basics of java/Basics.java
@@ -1,10 +1,11 @@
-**Count digits in a number**
+/* *Count digits in a number**
 SOLUTION 1:
 Approach: 
  -Store the integer in a variable X and initialize a counter variable to count the number of digits.
  -We know that in programming languages when we divide X by Y it will result in an integer (given both the variables are integers). For example, 133/10 will result in 13 similarly 1/10 will result in 0.
  -Using a for loop and above observation keep on dividing X by 10 and increment the count in every iteration when X equals 0 terminate the loop and the count will have the number of digits in N.
-public class tUf { 
+*/
+ public class tUf { 
     static int count_digits(int n)
     {
        int x = n; int count =0;
@@ -21,7 +22,7 @@ public class tUf {
         System.out.println("Number of digits in "+n+" is "+count_digits(n));
     } 
 }
-
+/*
 Time Complexity: O (n) where n is the number of digits in the given integer
 
 Space Complexity: O(1)
@@ -30,6 +31,7 @@ SOLUTION 2:
 
 -Convert the integer into a string.
 -Find the length of the string 
+*/
 
 import java.util.*;
 public class tUf {
@@ -44,6 +46,7 @@ public class tUf {
   }
 }
 
+/*
 Time Complexity: O (1) 
 
 Space Complexity: O(1)
@@ -59,6 +62,7 @@ Example :
 n = 12345
 log10(12345) = 4.091
 Integral part of 4.091 is 4 and 4 + 1 =  5 which is also the number of digits in 12345  
+*/
 
 import java.util.*;
 public class tUf { 
@@ -74,6 +78,7 @@ public class tUf {
         System.out.println("Number of digits in "+n+" is "+count_digits(n));
     } 
 }
+/*
 Time Complexity: O (1) 
-
 Space Complexity: O(1)
+*/
