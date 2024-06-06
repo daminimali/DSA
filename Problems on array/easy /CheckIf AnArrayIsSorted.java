2653141864d123878abdaddba79a472c2bffb8be
daminimali <148991044+daminimali@users.noreply.github.com>
2024-06-06 21:36:28 +0530

    Update and rename Check if an Array is Sorted to CheckIf AnArrayIsSorted.java

--- Problems on array/easy /CheckIf AnArrayIsSorted.java
+++ Problems on array/easy /CheckIf AnArrayIsSorted.java
@@ -1,4 +1,5 @@
-Problem Statement: Given an array of size n, write a program to check if the given array is sorted in (ascending / Increasing / Non-decreasing) order or not. If the array is sorted then return True, Else return False.
+/*
+ Problem Statement: Given an array of size n, write a program to check if the given array is sorted in (ascending / Increasing / Non-decreasing) order or not. If the array is sorted then return True, Else return False.
 
 Note: Two consecutive equal values are considered to be sorted.
 
@@ -27,8 +28,7 @@ The idea is pretty simple here, We will start with the element at the 0th index,
 If the picked element is smaller than or equal to all of its future values then we will move to the next Index/element until the whole array is traversed.
 If any of the picked elements is greater than its future elements, Then simply we will return False.
 If the size of the array is Zero or One i.e ( N = 0 or N = 1 ) or the entire array is traversed successfully then we will simply return True.
-
-Code:
+*/
 class TUF {
   static boolean isSorted(int arr[], int n) {
     for (int i = 0; i < n; i++) {
@@ -47,7 +47,7 @@ class TUF {
     System.out.println(isSorted(arr, n));
   }
 }
-
+/*
 Output: True
 
 Complexity Analysis
@@ -63,8 +63,7 @@ As we know that for a sorted array the previous of every element is smaller than
 So, Through this, we can conclude that if the previous element is smaller than or equal to the current element then. Then we can say that the two elements are sorted. If the condition is true for the entire array then the array is sorted.
 We will check every element with its previous element if the previous element is smaller than or equal to the current element then we will move to the next index.
 If the whole array is traversed successfully or the size of the given array is zero or one (i.e N = 0 or N = 1). Then we will return True else return False.
-
-Code:
+*/
 class TUF {
   static boolean isSorted(int arr[], int n) {
     for (int i = 1; i < n; i++) {
@@ -81,10 +80,11 @@ class TUF {
     System.out.println(isSorted(arr, n));
   }
 }
-
+/*
 Output: True
 
 Complexity Analysis
 Time Complexity: O(N)
 
 Space Complexity: O(1)
+*/
