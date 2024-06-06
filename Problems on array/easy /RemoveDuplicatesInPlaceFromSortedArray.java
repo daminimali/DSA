e0f7a1d79372ae3ae8dd90b8afdc2997debe739d
daminimali <148991044+daminimali@users.noreply.github.com>
2024-06-06 21:40:29 +0530

    Update and rename Remove Duplicates in-place from Sorted Array to RemoveDuplicatesInPlaceFromSortedArray.java

--- Problems on array/easy /RemoveDuplicatesInPlaceFromSortedArray.java
+++ Problems on array/easy /RemoveDuplicatesInPlaceFromSortedArray.java
@@ -1,4 +1,4 @@
-Problem Statement: Given an integer array sorted in non-decreasing order, remove the duplicates in place such that each unique element appears only once. The relative order of the elements should be kept the same.
+/* Problem Statement: Given an integer array sorted in non-decreasing order, remove the duplicates in place such that each unique element appears only once. The relative order of the elements should be kept the same.
 
 If there are k elements after removing the duplicates, then the first k elements of the array should hold the final result. It does not matter what you leave beyond the first k elements.
 
@@ -38,8 +38,7 @@ Put every element of the array in the set.
 Store size of the set in a variable K.
 Now put all elements of the set in the array from the starting of the array.
 Return K.
-
-Code:
+*/
 
 import java.util.*;
 public class Main {
@@ -64,6 +63,7 @@ public class Main {
         return k;
     }
 }
+/*
 
 Output: The array after removing duplicate elements is 1 2 3
 
@@ -82,8 +82,7 @@ Take a variable i as 0;
 Use a for loop by using a variable ‘j’ from 1 to length of the array.
 If arr[j] != arr[i], increase ‘i’ and update arr[i] == arr[j].
  After completion of the loop return i+1, i.e size of the array of unique elements.
-
-Code:
+*/
 
 import java.util.*;
 public class Main {
@@ -106,7 +105,7 @@ public class Main {
         return i + 1;
     }
 }
-
+/*
 Output:
 
 The array after removing duplicate elements is 1 2 3
@@ -115,3 +114,4 @@ Complexity Analysis
 Time Complexity: O(N)
 
 Space Complexity: O(1)
+*/
